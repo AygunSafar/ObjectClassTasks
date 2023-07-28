5e4385c10e31ed308bb1d6b898aa1e0a012e5f88
AygunSafar <aygunsafarova2000@gmail.com>
2023-07-28 17:12:27 +0400

    Added CarpetCostCalculator package, Created Calculator,Carpet and Floor classes for calculating the price of carpet

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import carpetCostCalculator.Calculator;
+import carpetCostCalculator.Carpet;
+import carpetCostCalculator.Floor;
 import cylinder.Circle;
 import cylinder.Cylinder;
 import poolArea.Cuboid;
@@ -30,6 +33,19 @@ public class Main {
         System.out.println("cylinder.height= " + cylinder.getHeight());
         System.out.println("cylinder.area= " + cylinder.getArea());
         System.out.println("cylinder.volume= " + cylinder.getVolume());
+
+        System.out.println("*****************************************************************");
+
+        // Carpet Cost Calculator Test Code
+
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
 
     }
 }
---
+++ src/carpetCostCalculator/Carpet.java
@@ -0,0 +1,14 @@
+package carpetCostCalculator;
+
+public class Carpet {
+
+    private double cost;
+
+    public Carpet(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost(){
+        return  this.cost;
+    }
+}
